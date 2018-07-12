f1e59debb968dfff6accd69150fa20f8c600f870
NarmathaSethu <41136347+NarmathaSethu@users.noreply.github.com>
2018-07-12 18:01:33 +0530

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int b=in.nextInt();
+	int c=in.nextInt();
+	System.out.println(Math.max(a, Math.max(b, c)));
+			in.close();
+		}
