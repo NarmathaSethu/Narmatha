c842e8689578291d26bd08af5a0dca3f12189ba8
NarmathaSethu <41136347+NarmathaSethu@users.noreply.github.com>
2018-07-16 18:16:16 +0530

    Create dot.java

---
+++ dot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+	    String str=in.next();
+	    System.out.println(str+".");
+    in.close();
+	}
+
+}
