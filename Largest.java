f82ded84c92770a14597aa3ba787d905adb0c667
NarmathaSethu <41136347+NarmathaSethu@users.noreply.github.com>
2018-07-21 12:01:45 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -3,16 +3,14 @@ public class Demo {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
      int n=in.nextInt();
-     int temp=0;
      int []a=new int[n];
      for(int i=0;i<n;i++){
     	 a[i]=in.nextInt();
      }
     Arrays.sort(a);
     for(int i=n-1;i>=0;i--){
-    	temp=(temp*10)+a[i];
+    	System.out.print(a[i]+"");
     }
-    System.out.println(temp);
     in.close();
 	}
 }
