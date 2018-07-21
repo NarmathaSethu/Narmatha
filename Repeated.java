6c7b3dc9156b126ec3a30465d60723865ec02b62
NarmathaSethu <41136347+NarmathaSethu@users.noreply.github.com>
2018-07-21 11:45:34 +0530

    Create Repeated.java

---
+++ Repeated.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Demo {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+     int n=in.nextInt();
+     int c=0;
+     HashSet<Integer> hs=new HashSet<Integer>();
+     int []a=new int[n];
+     for(int i=0;i<n;i++){
+    	 a[i]=in.nextInt();
+     }
+     for(int i=0;i<n;i++){
+    	 c=0;
+    	 for(int j=i;j<n;j++){
+    		if(a[i]==a[j]){
+    			c++;
+    		}
+    	 }
+    	 if(c>1){
+    	 hs.add(a[i]);
+    	 }
+     }
+     Iterator<Integer> itr=hs.iterator();
+     while(itr.hasNext()){
+    	 System.out.print(itr.next()+" ");
+     }
+    in.close();
+	}
+}
