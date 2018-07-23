8588f17c9fd8a64deaf040180db15e27b3b62225
NarmathaSethu <41136347+NarmathaSethu@users.noreply.github.com>
2018-07-23 21:17:38 +0530

    Update sort11.java

--- sort11.java
+++ sort11.java
@@ -1,17 +1,20 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 public class Vuram_pattern {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		int n=in.nextInt();
+		ArrayList<Integer> al=new ArrayList<Integer>();
 		int []a=new int[n];
 		for(int i=0;i<n;i++){
 			a[i]=in.nextInt();
+			al.add(a[i]);
 		}
-		Arrays.sort(a);
-		for(int i=0;i<n;i++){
-			System.out.print(a[i]+" ");
+		Collections.sort(al);
+		Iterator<Integer> itr=al.iterator();
+		while(itr.hasNext()){
+			System.out.print(itr.next()+" ");
 		}
+		
 in.close();
 	}
 
